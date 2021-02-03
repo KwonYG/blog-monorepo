072bf3b5ad8351fce9dbac3fbb730e5df3eaa615
KwonYG <andamirocll@gmail.com>
2021-02-03 21:46:32 +0900

    chore: set storybook addons(controls, docs)

--- packages/yg-ui/src/Button/Button.stories.tsx
+++ packages/yg-ui/src/Button/Button.stories.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Button from './'
+import {ButtonProps} from './Button.type'
+import {Story} from '@storybook/react'
 
 export default {
   title: 'Button',
+  component: Button,
 }
 
 const BASE_BUTTON =
   'rounded outline-none shadow py-3 px-12 font-normal uppercase tracking-wider text-lg bg-gray-500'
 const BASE_BUTTON_2 = 'py-3 px-12 font-normal  text-lg bg-gray-200'
 
-export const Button1 = () => <Button text="니뭐ㅋ" className={BASE_BUTTON} />
+const Template: Story<ButtonProps> = (args) => <Button {...args} />
 
-export const Button2 = () => <Button text="니뭐ㅎ" className={BASE_BUTTON_2} />
+export const Button1 = Template.bind({})
+
+Button1.args = {text: '알비 바보', className: BASE_BUTTON}
+
+export const Button2 = Template.bind({})
+Button2.args = {text: '니뭐ㅎ', className: BASE_BUTTON_2}
