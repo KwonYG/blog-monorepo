ca1713acea22a4350a832d1e9aad7f6e9e3cbff7
KwonYG <andamirocll@gmail.com>
2021-02-20 20:13:20 +0900

    chore: change card story

--- packages/yg-ui/src/Card/Card.stories.tsx
+++ packages/yg-ui/src/Card/Card.stories.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import Card from '.'
 import CardHeader from './CardHeader'
 import CardBody from './CardBody'
-import CardFooter from './CardFooter'
 import { CardProps } from './Card.type'
 import { Story } from '@storybook/react'
 
 export default {
-  title: 'Card',
+  title: 'Card(스타일작업 X, 옵션 시스템만)',
   component: Card,
 }
 
@@ -15,8 +14,21 @@ const Template: Story<CardProps> = (args) => <Card {...args} />
 
 export const BasicCard = Template.bind({})
 BasicCard.args = {
-  children: 'hahaha',
-  className: '',
+  className: 'p-4',
+  children: (
+    <>
+      <CardHeader className="mb-4">기본 카드 제목</CardHeader>
+      <CardBody>
+        <p>기본 카드 바디</p>
+      </CardBody>
+    </>
+  ),
+}
+
+BasicCard.argTypes = {
+  children: {
+    control: { disable: true },
+  },
 }
 
 export const ImageCard = Template.bind({})
@@ -26,12 +38,17 @@ ImageCard.args = {
     'http://www.busan.com/nas/data/content/image/2017/06/21/20170621000109_0.jpg',
   children: (
     <>
-      <CardHeader className="w-15 break-words text-white">
+      <CardHeader className="px-4 w-15 break-words text-white">
         유희열 미소는 천사미소
       </CardHeader>
-      <CardBody>
+      <CardBody className="p-4">
         <p className="text-white">이건 거의 알비급..</p>
       </CardBody>
     </>
   ),
+}
+ImageCard.argTypes = {
+  children: {
+    control: { disable: true },
+  },
 }
