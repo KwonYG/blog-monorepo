3b1c676bc0d8cea7907f61fe400ba948e05beefd
KwonYG <andamirocll@gmail.com>
2021-02-21 15:16:39 +0900

    chore: add comment to themeContext.tsx

--- packages/yg-ui/src/context/ThemeContext.tsx
+++ packages/yg-ui/src/context/ThemeContext.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import defaultTheme from '../themes/default'
 import { DefaultTheme } from '../themes/theme.types'
 
+// TODO: 안쓰는 컨텍스트. 임시로 남겨두고 현재 테마 적용 방식에 이슈가 안생기면 삭제하기
 interface ThemeContextInterface<T> {
   theme: T
 }
