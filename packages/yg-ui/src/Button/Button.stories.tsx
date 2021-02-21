b092fa4048481112746d5d2e1785a1818de039ee
KwonYG <andamirocll@gmail.com>
2021-02-21 15:04:29 +0900

    feat: add yg-card-header yg-card-body

--- packages/yg-ui/src/Button/Button.stories.tsx
+++ packages/yg-ui/src/Button/Button.stories.tsx
@@ -8,14 +8,10 @@ export default {
   component: Button,
 }
 
-const BASE_BUTTON =
-  'rounded outline-none shadow py-3 px-12 font-normal uppercase tracking-wider text-lg bg-gray-500'
-const BASE_BUTTON_2 = 'py-3 px-12 font-normal  text-lg bg-gray-200'
-
 const Template: Story<ButtonProps> = (args) => <Button {...args} />
 
 export const Button1 = Template.bind({})
-Button1.args = { text: '알비 굿', className: BASE_BUTTON }
+Button1.args = { text: '알비 굿', className: '' }
 
 export const Button2 = Template.bind({})
-Button2.args = { text: '니뭐ㅎ', className: BASE_BUTTON_2 }
+Button2.args = { text: '니뭐ㅎ', className: '' }
--- packages/yg-ui/src/Button/index.tsx
+++ packages/yg-ui/src/Button/index.tsx
@@ -2,14 +2,8 @@ import React from 'react'
 import { ButtonProps } from './Button.type'
 
 function Button({ text, className }: ButtonProps) {
-  const BASE_BUTTON =
-    'rounded outline-none shadow py-3 px-12 font-normal uppercase tracking-wider text-lg bg-gray-500'
-
   return (
-    <button
-      data-testid="button-component"
-      className={className ? className : BASE_BUTTON}
-    >
+    <button data-testid="button-component" className={`yg-button ${className}`}>
       {text}
     </button>
   )
